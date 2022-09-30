49511398fdc5d26d734b41c3612ccf3546dd2909
Sam Jones <sam0jones0@gmail.com>
2022-09-30 15:39:21 +0100

    Exercise 7a

---
+++ ReactJSQuickLabs/b-static-version/starter/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-auto py-3 container text-center">
+      &copy; QA Ltd 2019-
+    </footer>
+  );
+}
